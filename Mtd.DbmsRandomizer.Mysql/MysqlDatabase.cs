d658b855702f4bda316dc109826ef9b578074e09
Adam Grabski <adam.gr@outlook.com>
2021-05-21 23:32:51 +0200

    .

--- Mtd.DbmsRandomizer.Mysql/MysqlDatabase.cs
+++ Mtd.DbmsRandomizer.Mysql/MysqlDatabase.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
@@ -36,8 +38,30 @@ namespace Mtd.DbmsRandomizer.Mysql
 			var deleteCommand = _connection.CreateCommand();
 			deleteCommand.CommandText = $"delete from {reader.GetSchemaTable().TableName}";
 			await deleteCommand.ExecuteNonQueryAsync(cc);
-			var bulkCopy = new MySqlBulkLoader(_connection);
-			await bulkCopy.LoadAsync(reader, cc);
+			var file = GetTemporaryFilePath(reader);
+			try
+			{
+				var bulkCopy = new MySqlBulkLoader(_connection)
+				{
+					TableName = reader.GetSchemaTable().TableName,
+					FieldTerminator = ",",
+					LineTerminator = "\n",
+					FileName = file,
+
+				};
+			await bulkCopy.LoadAsync(cc);
+			}
+			finally
+			{
+				File.Delete(file);
+			}
+		}
+
+		private string GetTemporaryFilePath(IDataReader reader)
+		{
+			var path = Path.GetRandomFileName();
+			ToCsv(reader, path);
+			return path;
 		}
 
 		public string FormatLiteral(object literal)
@@ -47,5 +71,30 @@ namespace Mtd.DbmsRandomizer.Mysql
 
 		public DbConnection Connection => _connection;
 		public DbType Type => DbType.MySql;
+
+		public void ToCsv(IDataReader reader, string filename)
+		{
+			int nextResult = 0;
+			do
+			{
+				var filePath = filename;
+				using (var writer = new StreamWriter(filePath))
+				{
+					writer.WriteLine(string.Join(",", Enumerable.Range(0, reader.FieldCount).Select(reader.GetName).ToList()));
+					int count = 0;
+					while (reader.Read())
+					{
+						writer.WriteLine(string.Join(",", Enumerable.Range(0, reader.FieldCount).Select(reader.GetValue).ToList()));
+						if (++count % 100 == 0)
+						{
+							writer.Flush();
+						}
+					}
+				}
+
+				filename = string.Format("{0}-{1}", filename, ++nextResult);
+			}
+			while (reader.NextResult());
+		}
 	}
 }
